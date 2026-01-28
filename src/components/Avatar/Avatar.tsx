e25815a0a77e643f2679fff3e7bd42117d80d776
Antonio Navarro <dojeDulDev@Proton.me>
2026-01-28 08:10:27 +0100

    feat(avatar): create avatar component

---
+++ src/components/Avatar/Avatar.tsx
@@ -0,0 +1,19 @@
+interface AvatarProps {
+  color: string
+  radius: number
+  name: string
+}
+
+const Avatar = ({color, radius, name}: AvatarProps) => {
+  return (
+    
+    
+    <div>
+      
+    </div>
+
+
+  )
+}
+
+export default Avatar
