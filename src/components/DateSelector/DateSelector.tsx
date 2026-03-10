24f8e197a76aee7db86a74ee73629bea95bee363
Antonio Navarro <dojeDulDev@Proton.me>
2026-03-10 18:10:29 +0100

    feat: implement logic to retrieve next week and next weekend dates

--- src/components/DateSelector/DateSelector.tsx
+++ src/components/DateSelector/DateSelector.tsx
@@ -1,22 +1,31 @@
 import styles from "./DateSelector.module.css";
+import cn from "classnames";
 import { Icons } from "../Icons";
 import { useState, useEffect } from "react";
-import {getDayOfWeek, getMonthName} from '../../utils/dateUtils'
-import type {DateState} from '../../utils/dateUtils'
-
+import {
+  getDayOfWeek,
+  getMonthName,
+  daysOfWeekMon,
+  generateMonth,
+  getNextWeekEnd,
+} from "../../utils/DateUtils";
+import type { DateState, daysOfMonth } from "../../utils/DateUtils";
 
 interface DateSelectorProps {
   handleTaskForm: (name: string, value: Date | null) => void;
 }
-
 
 const DateSelector = ({ handleTaskForm }: DateSelectorProps) => {
   const [dates, setDates] = useState<DateState>({
     today: new Date(),
     tomorrow: new Date(),
     nextWeek: new Date(),
-    nextMonth: new Date(),
+    nextWeekend: new Date(),
+    currentMonth: [],
   });
+
+  const [viewDate, setViewDate] = useState(new Date());
+  const [viewMonth, setViewMonth] = useState(new Date().getMonth());
 
   useEffect(() => {
     let today = new Date();
@@ -24,16 +33,31 @@ const DateSelector = ({ handleTaskForm }: DateSelectorProps) => {
     let nextWeek = new Date();
     tomorrow.setDate(tomorrow.getDate() + 1);
     nextWeek.setDate(nextWeek.getDate() + 7);
-    let nextMonth = new Date();
-    nextMonth.setDate(nextMonth.getDate() + 30);
+
+    console.log("today is ", today);
 
     setDates({
       today: today,
       tomorrow: tomorrow,
       nextWeek: nextWeek,
-      nextMonth: nextMonth,
+      nextWeekend: getNextWeekEnd(today),
+      currentMonth: generateMonth(today),
     });
   }, []);
+
+  const changeMonth = (offset: number) => {
+    // Al hacer return explicito se crea una nueva instancia y se asegura que actualiza la interfaz
+    setViewDate((prev) => {
+      const nextViewDate = new Date(
+        prev.getFullYear(),
+        prev.getMonth() + offset,
+        1,
+      );
+      return nextViewDate;
+    });
+
+    setDates({ ...dates, currentMonth: generateMonth(viewDate) });
+  };
 
   return (
     <>
@@ -55,27 +79,55 @@ const DateSelector = ({ handleTaskForm }: DateSelectorProps) => {
           <button
             type="button"
             onClick={() => handleTaskForm("dueDate", dates.nextWeek)}
-            >
+          >
             <span>Próxima semana</span>
             <span>{getDayOfWeek(dates.nextWeek.getDay())}</span>
           </button>
           <button
             type="button"
-            onClick={() => handleTaskForm("dueDate", dates.nextMonth)}
+            onClick={() => handleTaskForm("dueDate", dates.nextWeekend)}
           >
-            <span>Próxima mes</span>
-            <span>{getDayOfWeek(dates.nextMonth.getDay())}</span>
+            <span>Este fin de semana</span>
+            <span>{getDayOfWeek(dates.nextWeekend.getDay())}</span>
           </button>
 
           <div className={styles.datePickerMonthNavigator}>
             <span>{getMonthName(dates.today.getMonth())}</span>
             <div className={styles.datePickerNavButtons}>
-              {<Icons name="ArrowLeft" />}
-              {<Icons name="ArrowRight" />}
+              <button type="button" onClick={() => changeMonth(-1)}>
+                {<Icons name="ArrowLeft" />}
+              </button>
+              <button type="button" onClick={() => changeMonth(1)}>
+                {<Icons name="ArrowRight" />}
+              </button>
             </div>
           </div>
           {/* Calendar */}
-          <div></div>
+          <div className={styles.datePickerCalendarGrid}>
+            {daysOfWeekMon.map((day) => {
+              return (
+                <>
+                  <div> {day.slice(0, 1)} </div>
+                </>
+              );
+            })}
+            {dates.currentMonth.map((day) => {
+              return (
+                <>
+                  <button
+                    type="button"
+                    disabled={day.isDisable}
+                    className={cn(styles.datePickerCell, {
+                      [styles.currentDay]: day.isCurrentDay,
+                      [styles.datePickerDisabled]: day.isDisable,
+                    })}
+                  >
+                    {day.isVisible && day.day}
+                  </button>
+                </>
+              );
+            })}
+          </div>
         </div>
       </div>
       ;
--- src/utils/DateUtils.ts
+++ src/utils/DateUtils.ts
@@ -2,7 +2,15 @@ export interface DateState {
   today: Date;
   tomorrow: Date;
   nextWeek: Date;
-  nextMonth: Date;
+  nextWeekend: Date;
+  currentMonth: daysOfMonth[];
+}
+
+export interface daysOfMonth {
+  day: number;
+  isDisable: boolean;
+  isCurrentDay: boolean;
+  isVisible: boolean;
 }
 
 export const daysOfWeek = [
@@ -14,6 +22,17 @@ export const daysOfWeek = [
   "Viernes",
   "Sábado",
 ];
+
+export const daysOfWeekMon = [
+  "Lunes",
+  "Martes",
+  "Miércoles",
+  "Jueves",
+  "Viernes",
+  "Sábado",
+  "Domingo",
+];
+
 export const monthsOfYear = [
   "Enero",
   "Febrero",
@@ -30,6 +49,8 @@ export const monthsOfYear = [
 ];
 
 export const getDayOfWeek = (day: number): string => {
+  console.log("getDayOfWeek day: ", day);
+
   return daysOfWeek[day].slice(0, 3);
 };
 
@@ -37,3 +58,75 @@ export const getMonthName = (month: number): string => {
   return monthsOfYear[month].slice(0, 3);
 };
 
+export const getMonday = (currentDate: Date): number => {
+  const dayNumber = currentDate.getDay();
+  const dayOfMonth = currentDate.getDate();
+  const daysToSubtract = dayNumber === 0 ? 6 : dayNumber - 1;
+
+  return dayOfMonth - daysToSubtract;
+};
+
+/*
+ Obtengo la fecha del siguiente sábado. 
+*/
+
+export const getNextWeekEnd = (date: Date): Date => {
+  const dayNumber = date.getDay();
+  const daysToAdd = dayNumber === 0 ? 6 : 6 - dayNumber;
+  const d = new Date(date);
+
+  d.setDate(d.getDate() + daysToAdd);
+  return d;
+};
+
+export const getLastDay = (date: Date): number => {
+  const year = date.getFullYear();
+  const month = date.getMonth();
+
+  console.log("month ", month);
+
+  const lastDay = new Date(year, month + 1, 0).getDate();
+
+  console.log("lastDay ", lastDay);
+
+  return lastDay;
+};
+
+// Genera los dias del mes a partir de la semana del dia acutal.
+// Por ejemplo si hoy fuese cuatro (miercoles), empieza a generar desde el 2 lunes hasta el final del mes.
+export const generateMonth = (date: Date): daysOfMonth[] => {
+  console.log("generateMonth");
+
+  const firstDay = getMonday(date);
+  const lastDay = getLastDay(date);
+  const currentDay = date.getDate();
+
+  console.log("firstDay ", firstDay);
+  console.log("lastDay ", lastDay);
+
+  let daysOfMonth = [];
+
+  let isCurrentDay = false;
+
+  for (let i = firstDay; i <= lastDay; i++) {
+    let isDisable = false;
+    let isVisible = true;
+
+    if (i < currentDay) {
+      isDisable = true;
+    }
+
+    isCurrentDay = i == currentDay;
+
+    // Los dias del mes anterior no se muestran.
+    if (i < 1) {
+      isVisible = false;
+    }
+
+    daysOfMonth.push({ day: i, isDisable, isCurrentDay, isVisible });
+  }
+
+  // console.table(daysOfMonth);
+
+  return daysOfMonth;
+};
