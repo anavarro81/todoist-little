b808823c4235a6a95fe4371049e15887c7ab7f3b
Antonio Navarro <dojeDulDev@Proton.me>
2026-03-31 10:53:18 +0200

    feat: implement interface for RepeatSelector component data management

--- src/components/DateSelector/RepeatSelector.tsx
+++ src/components/DateSelector/RepeatSelector.tsx
@@ -1,6 +1,18 @@
+import { useState } from "react";
 import styles from "./DateSelector.module.css";
 
 const RepeatSelector = () => {
+  interface repetionConfig {
+    isRepeating: boolean;
+    RepeatType: "dayly" | "weekly" | "monthly" | "yearly";
+    interval: number;
+    repeatValue: string;
+    endStretegy: "endless" | "by_date" | "by_coount";
+    endDate: Date | null;
+    ocurrenceLimit: number | null;
+  }
+
+  // TODO Quitar cuando tenga las valores. 
   const repetition = {
     semanal: "martes",
     mensual: "día 31",
@@ -8,23 +20,30 @@ const RepeatSelector = () => {
     diaSemana: "Lun-Vie",
   };
 
+  const [repetion, setRepetion] = useState<repetionConfig>(
+    {
+        isRepeating: false,
+        RepeatType: "dayly",
+        interval: 1,
+        repeatValue: "",
+        endStretegy: "endless",
+        endDate: null,
+        ocurrenceLimit: null
+    })
+    
   return (
     <div className={styles.repeatSelectorContainer}>
       <button> Diario </button>
       <button>
-        {" "}
         Semanal <span> ({repetition.semanal}) </span>
       </button>
       <button>
-        {" "}
         Mensual <span> ({repetition.mensual}) </span>
       </button>
       <button>
-        {" "}
-        Anual <span> ({repetition.anual}) </span>{" "}
+        Anual <span> ({repetition.anual}) </span>
       </button>
       <button>
-        {" "}
         Dias de la semana <span> ({repetition.diaSemana}) </span>
       </button>
     </div>
---
+++ src/readme.md
@@ -0,0 +1,6 @@
+Estoy creando una aplicación de gestion de tareas, similar a Todoist, pero más sencilla. 
+
+La aplicación la estoy haciendo en React con Typescript. El backend es con MongoDb + Node. 
+
+
+
